d76f127404a083bd7cc2d973bb2582cf5a0aaa24
Derick Jansen <derick.jansen@vocusgroup.co.nz>
2021-11-11 12:25:51 +1300

    CORS

--- app/main.py
+++ app/main.py
@@ -1,15 +1,22 @@
-from fastapi import FastAPI
-
-from app.database.base import init_db
-
+from fastapi import FastAPI 
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers.user import router as user_router
 from app.routers.post import router as post_router
 from app.routers.auth import router as auth_router
 from app.routers.vote import router as vote_router
 
-init_db()
+CORS_ORIGINS = ["*"]
 
 app = FastAPI()
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins = CORS_ORIGINS,
+  allow_credentials = True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
 app.include_router(user_router)
 app.include_router(post_router)
 app.include_router(auth_router)
