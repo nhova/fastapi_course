92a58bcc2fcb9b97cc3922794e123cc571b7b4ce
Derick Jansen <derick.jansen@vocusgroup.co.nz>
2021-11-13 20:30:26 +1300

    fix test_create_post

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -38,7 +38,7 @@ def test_authed_user_get_one_posts(authorized_client, test_posts):
 def test_create_post(authorized_client, test_user,  title, content, published):
   res = authorized_client.post("/posts/", json = { "title": title,
                                                     "content": content,
-                                                    "publised": published })
+                                                    "published": published })
   created_post = PostResponse(**res.json())
   assert res.status_code == 201
   assert created_post.title == title
